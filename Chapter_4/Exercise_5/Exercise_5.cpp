233a391f0b9257491932a99e507bc14f04bbccef
MajedMZCR <67710899+MajedMZCR@users.noreply.github.com>
2023-11-11 14:53:51 +0200

    Add files via upload

--- Chapter_4/Exercise_5/Exercise_5.cpp
+++ Chapter_4/Exercise_5/Exercise_5.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <ctype.h>
 
+using namespace std;
+
+int main()
+{
+    char array[80];
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    while (in.getline(array, 80))
+    {
+        cout << array << endl;
+        for (int i = 0; i < 80 ; i++)
+          array[i] = toupper (array[i]);
+        out << array << endl;
+    }
+
+    in.close();
+    out.close();
+
+    return 0;
+}
